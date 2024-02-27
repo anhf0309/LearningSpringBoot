ff59fd5a3728caf9f9934051b360f56659c2d697
anhf0309 <anhf0309@gmail.com>
2024-02-27 00:21:15 -0800

    309.5.4

--- ExerciseOne_Rest/src/main/java/com/perscholas/ExerciseOne_Rest/TestController/MyController.java
+++ ExerciseOne_Rest/src/main/java/com/perscholas/ExerciseOne_Rest/TestController/MyController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("home")
 public class MyController {
-    @RequestMapping({"/login"})   // either type '/' or index
+    @RequestMapping({"/login"})
     public String showlogin()
     {
         return "inboxpage";
