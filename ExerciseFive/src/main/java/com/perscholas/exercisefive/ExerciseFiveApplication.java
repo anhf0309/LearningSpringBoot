50df08836f3c807570b3a69bac5e5c7bc6bf7bba
anhf0309 <anhf0309@gmail.com>
2024-02-27 08:15:35 -0800

    309.5.5 done

---
+++ ExerciseFive/src/main/java/com/perscholas/exercisefive/ExerciseFiveApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.exercisefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseFiveApplication.class, args);
+    }
+
+}
---
+++ ExerciseFive/src/test/java/com/perscholas/exercisefive/ExerciseFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.exercisefive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
